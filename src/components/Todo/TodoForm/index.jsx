35838349e9a4bcdc2a68c99dca7abb7036949d37
Bilous Oleg <olezhka82802@gmail.com>
2026-05-09 21:14:24 +0300

    Add advanced level first working variant

--- src/components/Todo/TodoForm/index.jsx
+++ src/components/Todo/TodoForm/index.jsx
@@ -1,24 +1,63 @@
 import { useContext, useState } from 'react';
+import * as yup from 'yup';
+import classNames from 'classnames';
 import { TodoContext } from '../../../contexts';
+import CONSTANTS from '../../../constants';
 import styles from './TodoForm.module.sass';
 
-const INITIAL_VALUE = '';
+const [MAX_LENGTH, MIN_LENGTH, INITIAL_VALUE] = CONSTANTS;
+
+// yup-схема для подальшої валідації
+const TASK_SCHEMA = yup.object({
+  task: yup
+    .string()
+    .trim()
+    .required('Task is required')
+    .min(MIN_LENGTH, `Minimum ${MIN_LENGTH} characters required`)
+    // Резервна перевірка на максимальну кількість слів, рівну 150 (оскільки ліміт встановлений через maxLength атрибут)
+    .max(MAX_LENGTH, `Maximum ${MAX_LENGTH} characters`),
+});
 
 function TodoForm() {
   const { addTask } = useContext(TodoContext); // Хук контексту для отримання функції додавання завдання
   const [task, setTask] = useState(INITIAL_VALUE);
-  // isTaskValid
+  const [error, setError] = useState(null);
 
   const handleTaskChange = ({ target: { value } }) => {
     setTask(value);
-    // isTaskValid
+
+    // Асинхронна валідація схеми (якби полів було більше, доречно було б використати validateAt)
+    // не знаю, наскільки це безпечно та ефективно (асинхронно), але зробив так
+    TASK_SCHEMA.validate({
+      task: value,
+    })
+      .then(() => {
+        setError(null);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTask(task);
-    setTask(INITIAL_VALUE);
+
+    TASK_SCHEMA.validate({
+      task,
+    })
+      .then(() => {
+        addTask(task);
+        setTask(INITIAL_VALUE);
+        setError(null);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
   };
+
+  const inputClassNames = classNames(styles.formInput, {
+    [styles.invalidInput]: error,
+  });
 
   return (
     <form action="" onSubmit={handleSubmit} className={styles.todoForm}>
@@ -26,7 +65,8 @@ function TodoForm() {
         type="text"
         value={task}
         name="taskInput"
-        className={styles.formInput}
+        className={inputClassNames}
+        maxLength={MAX_LENGTH}
         onChange={handleTaskChange}
         placeholder="Type here"
         autoFocus
@@ -34,6 +74,7 @@ function TodoForm() {
       <button type="submit" className={styles.submitBtn}>
         Add
       </button>
+      <div className={styles.invalidMsg}>{error}</div>
     </form>
   );
 }
